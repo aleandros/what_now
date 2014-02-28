2f8fac971b67f96f71ebb8145fb35872963011b1
Edgar Cabrera <edgar.cabrera.rivas@gmail.com>
2014-02-28 16:04:55 -0600

    Find todos inside directories

---
+++ lib/what_now.rb
@@ -0,0 +1,29 @@
+# What_now definitions
+
+Todo = Struct.new :text, :path, :line
+
+module WhatNow
+  class << self
+    def search_line(line, line_number, path)
+      text = /TODO\s([\w\s]+)/.match(line)
+      Todo.new(text[1], line_number, path) if text
+    end
+
+    def search_file(path)
+      todos = []
+      File.open(path).each_with_index do |line, i|
+        result = search_line(line, path, i+1)
+        todos << result if result
+      end
+      todos
+    end
+
+    def search_directory(pattern)
+      todos = []
+      Dir[pattern].each do |file|
+        todos = todos + search_file(file)
+      end
+      todos
+    end
+  end
+end
---
+++ spec/what_now_spec.rb
@@ -0,0 +1,57 @@
+require_relative 'spec_helper'
+
+describe Todo do
+  describe 'public interface' do
+    subject { Todo.new('do stuff', './stuff', 10) }
+    it { subject.must_respond_to :text }
+    it { subject.must_respond_to :path }
+    it { subject.must_respond_to :line }
+  end
+end
+
+describe WhatNow do
+  describe '#search_line' do
+    subject do
+      WhatNow.search_line('TODO this is a todo', 1, '.')
+    end
+
+    it 'correctly extracts text from todo' do
+      subject.text.must_match 'this is a todo'
+    end
+
+    it 'returns nil with a non todo line' do
+      WhatNow.search_line('this is not a todo', 1, '.')
+        .must_be_nil 
+    end
+  end
+
+  describe '#search_file' do
+    subject do
+      path = File.dirname(__FILE__) + '/example_file.txt'
+      WhatNow.search_file(path)
+    end
+
+    it 'found 2 todos' do
+      subject.length.must_equal 2
+    end
+
+    it 'found the correct todos' do
+      subject[0].text.must_match 'this is a todo'
+      subject[0].line.must_equal 2
+      subject[1].text.must_match 'but this is also a todo'
+      subject[1].line.must_equal 5
+    end
+  end
+
+  describe '#search_directory' do
+    it 'considers only specified pattern' do
+      results = WhatNow.search_directory(File.dirname(__FILE__)+'/**/*.txt')
+      results.length.must_equal 2
+    end
+
+    it 'returns empty array if nothing was found' do
+      results = WhatNow.search_directory(File.dirname(__FILE__)+'/**/*.mooo')
+      results.must_equal []
+    end
+  end
+end
