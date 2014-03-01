e09c491bfa4601613e77f9051d499a3cacd48fca
Christian Bundy <me@christianbundy.com>
2014-02-28 22:08:47 -0800

    Make regex case-insensitive

--- lib/what_now.rb
+++ lib/what_now.rb
@@ -7,7 +7,7 @@ Todo = Struct.new :text, :path, :line
 module WhatNow
   class << self
     def search_line(line, line_number, path)
-      text = /TODO[:\s]*\s(.+)$/.match(line)
+      text = /TODO[:\s]*\s(.+)$/i.match(line)
       Todo.new(text[1], line_number, path) if text
     end
 
